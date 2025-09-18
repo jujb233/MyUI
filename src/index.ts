a96df9127bca8546751eab031827e08727587e41
jujb233 <jujiangbo@whc.edu.cn>
2025-09-18 11:30:38 +0800

    feat: 更新版本号格式，修正package.json中的版本号并移除冗余的版本信息

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,3 @@ export { default as MyButton } from './Components/MyUI/MyButton'
 
 // 导出组件类型  
 export type { MyButtonProps } from './Components/MyUI/MyButton'
-
-// 版本信息
-
-export const version = '0.1.19.3.pre0'
